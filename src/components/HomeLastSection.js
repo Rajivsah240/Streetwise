0f36451866d526fc84b3167d98f01e973c260af3
rajiv <rajivsah240@gmail.com>
2023-06-18 13:55:33 +0530

    Rising Stars Card Added

--- src/components/HomeLastSection.js
+++ src/components/HomeLastSection.js
@@ -5,11 +5,157 @@ const HomeLastSection = () => {
   return (
     <Stack mt={"150px"}>
       <Stack
+        flexDirection={'row'}
+        height={"100vh"}
         borderTop={"1px solid white"}
+        alignItems={"center"}
+        className="rising-stars"
+        sx={{
+          "&:hover": {
+            background: "linear-gradient(45deg, #851DE0, transparent)",
+            border: "none",
+            color: "white",
+            "& .rising-stars-typo": {
+              color: "white",
+            },
+            "& .rising-stars-button": {
+              backgroundColor: "white",
+              color: "#851DE0",
+            },
+          },
+        }}
+      >
+        <Stack
+          flexDirection={"column"}
+          alignItems={"center"}
+          padding={"40px"}
+          marginLeft={"30px"}
+          width={"25%"}
+        >
+          <Typography
+            className="rising-stars-typo"
+            color={"#851DE0"}
+            fontSize={"50px"}
+            fontFamily={"Montserrat"}
+            fontWeight={"800"}
+          >
+            Meet The Rising Stars
+          </Typography>
+          <Button
+            className="rising-stars-button"
+            sx={{
+              mt: "10px",
+              color: "white",
+              borderRadius: "45px",
+              padding: "10px",
+              backgroundColor: "#851De0",
+              textTransform: "none",
+              width: "30%",
+              fontWeight: "400",
+            }}
+          >
+            Learn More
+          </Button>
+        </Stack>
+        <Stack flexDirection={'row'}>
+          <figure class="snip1559">
+            <div class="profile-image">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample2.jpg"
+                alt="profile-sample2"
+              />
+            </div>
+            <figcaption>
+              <h3>Sue Shei</h3>
+              <h5>Founder</h5>
+              <p>
+                Which is worse, that everyone has his price, or that the price
+                is always so low.
+              </p>
+              <div class="icons">
+                <a href="#">
+                  <i class="ion-social-reddit"></i>
+                </a>
+                <a href="#">
+                  {" "}
+                  <i class="ion-social-twitter"></i>
+                </a>
+                <a href="#">
+                  {" "}
+                  <i class="ion-social-vimeo"></i>
+                </a>
+              </div>
+            </figcaption>
+          </figure>
+          <figure class="snip1559">
+            <div class="profile-image">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample7.jpg"
+                alt="profile-sample7"
+              />
+            </div>
+            <figcaption>
+              <h3>Will Barrow</h3>
+              <h5>Web Designer</h5>
+              <p>
+                I'm killing time while I wait for life to shower me with meaning
+                and happiness.
+              </p>
+              <div class="icons">
+                <a href="#">
+                  <i class="ion-social-reddit"></i>
+                </a>
+                <a href="#">
+                  {" "}
+                  <i class="ion-social-twitter"></i>
+                </a>
+                <a href="#">
+                  {" "}
+                  <i class="ion-social-vimeo"></i>
+                </a>
+              </div>
+            </figcaption>
+          </figure>
+          <figure class="snip1559">
+            <div class="profile-image">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample6.jpg"
+                alt="profile-sample6"
+              />
+            </div>
+            <figcaption>
+              <h3>Indigo Violet</h3>
+              <h5>Public Relations</h5>
+              <p>
+                The only skills I have the patience to learn are those that have
+                no real application in life.{" "}
+              </p>
+              <div class="icons">
+                <a href="#">
+                  <i class="ion-social-reddit"></i>
+                </a>
+                <a href="#">
+                  {" "}
+                  <i class="ion-social-twitter"></i>
+                </a>
+                <a href="#">
+                  {" "}
+                  <i class="ion-social-vimeo"></i>
+                </a>
+              </div>
+            </figcaption>
+          </figure>
+        </Stack>
+      </Stack>
+
+      <Stack
+        borderTop={"1px solid white"}
+        height={"100vh"}
+        justifyContent={"center"}
         className="com-event-1"
         sx={{
           "&:hover": {
-            backgroundColor: "#EB5939",
+            background: "linear-gradient(45deg, #EB5939, transparent)",
             color: "white",
             border: "none",
             "& .com-event-typography": {
@@ -26,7 +172,7 @@ const HomeLastSection = () => {
           flexDirection={"column"}
           alignItems={"center"}
           padding={"40px"}
-          paddingLeft={"80px"}
+          marginLeft={"30px"}
           width={"25%"}
         >
           <Typography
@@ -57,13 +203,16 @@ const HomeLastSection = () => {
       </Stack>
 
       <Stack
+        justifyContent={"center"}
+        height={"100vh"}
         borderTop={"1px solid white"}
         alignItems={"flex-end"}
         className="exclusive-deals"
         sx={{
           "&:hover": {
-            backgroundColor: "#EB5939",
-            border:'none',
+            //backgroundColor: "#EB5939",
+            background: "linear-gradient(315deg, #EB5939, transparent)",
+            border: "none",
             color: "white",
             "& .exclusive-deals": {
               color: "white",
@@ -80,8 +229,8 @@ const HomeLastSection = () => {
           justifyItems={"flex-end"}
           alignItems={"center"}
           padding={"40px"}
-          paddingLeft={"80px"}
-          width={"25%"}
+          marginRight={"30px"}
+          width={"30%"}
         >
           <Typography
             className="exclusive-deals"
@@ -106,58 +255,6 @@ const HomeLastSection = () => {
             }}
           >
             Shop Now
-          </Button>
-        </Stack>
-      </Stack>
-
-      <Stack
-        borderTop={"1px solid white"}
-        className="rising-stars"
-        sx={{
-          "&:hover": {
-            backgroundColor: "#851DE0",
-            border:'none',
-            color: "white",
-            "& .rising-stars-typo": {
-              color: "white",
-            },
-            "& .rising-stars-button": {
-              backgroundColor: "white",
-              color: "#851DE0",
-            },
-          },
-        }}
-      >
-        <Stack
-          flexDirection={"column"}
-          alignItems={"center"}
-          padding={"40px"}
-          paddingLeft={"80px"}
-          width={"25%"}
-        >
-          <Typography
-            className="rising-stars-typo"
-            color={"#851DE0"}
-            fontSize={"50px"}
-            fontFamily={"Montserrat"}
-            fontWeight={"800"}
-          >
-            Meet The Rising Stars
-          </Typography>
-          <Button
-            className="rising-stars-button"
-            sx={{
-              mt: "10px",
-              color: "white",
-              borderRadius: "45px",
-              padding: "10px",
-              backgroundColor: "#851De0",
-              textTransform: "none",
-              width: "30%",
-              fontWeight: "400",
-            }}
-          >
-            Learn More
           </Button>
         </Stack>
       </Stack>
